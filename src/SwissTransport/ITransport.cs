f504553f10fb628bfc206068fabc542d25ab9d98
Rafael Estermann <rafi_estermann@hotmail.ch>
2017-11-21 11:52:48 +0100

    Initial

---
+++ src/SwissTransport/ITransport.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SwissTransport
+{
+    public interface ITransport
+    {
+        Stations GetStations(string query);
+        StationBoardRoot GetStationBoard(string station, string id);
+        Connections GetConnections(string fromStation, string toStattion, DateTime datetime);
+    }
+}
---
+++ tests/SwissTransportTest/TransportTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SwissTransport
+{
+    [TestClass]
+    public class TransportTest
+    {
+        private ITransport testee;
+
+        [TestMethod]
+        public void Locations()
+        {
+            testee = new Transport();
+            var stations = testee.GetStations("Sursee,");
+
+            Assert.AreEqual(10, stations.StationList.Count);
+        }
+
+        [TestMethod]
+        public void StationBoard()
+        {
+            testee = new Transport();
+            var stationBoard = testee.GetStationBoard("Sursee", "8502007");
+
+            Assert.IsNotNull(stationBoard);
+        }
+
+        [TestMethod]
+        public void Connections()
+        {
+            testee = new Transport();
+            var connections = testee.GetConnections("Sursee", "Luzern", DateTime.Now);
+
+            Assert.IsNotNull(connections);
+        }
+    }
+}
